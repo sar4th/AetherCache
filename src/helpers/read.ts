e492f3113fda1a20b672460d4559b621d4e0d125
sar4th <sarathrajklm0475@gmail.com>
2025-01-26 13:41:01 +0530

    reafactor : class refactor

---
+++ src/helpers/read.ts
@@ -0,0 +1,18 @@
+const { readFile } = require("fs/promises");
+const path = require("path");
+
+export async function readFileFromDisk(filePath: string) {
+  const filePath2 = path.join(
+    "C:",
+    "Users",
+    "sarat",
+    "microDB",
+    "dataStore.json"
+  );
+  try {
+    const data = await readFile(filePath2);
+    return data.toString();
+  } catch (error) {
+    console.error(error);
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,1 @@
+export type flags = "save" | "update" | "delete";
