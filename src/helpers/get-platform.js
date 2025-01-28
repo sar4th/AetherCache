3ae5884650df78952e73da1aab1c904da676ea98
sarathdev <sarathrajkrla@gmail.com>
2025-01-28 18:06:27 +0530

    feat : os in wip

---
+++ src/helpers/get-platform.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var node_process_1 = require("node:process");
+console.log("This platform is ".concat(node_process_1.platform));
