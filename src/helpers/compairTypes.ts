319c17c854e6a9c9754929337e13aa4e6430b0fa
sarathdev <sarathrajkrla@gmail.com>
2025-01-30 18:15:19 +0530

    feat : disk data sync

--- src/helpers/compairTypes.ts
+++ src/helpers/compairTypes.ts
@@ -1,29 +1,14 @@
-let obj2 = {
-  name: "string",
-  age: "string",
-  email: "string",
-};
-let obj1 = {
-  name: "sarath",
-  age: 12,
-  email: "s@mail",
-};
 export function compareTypes(value: any, schema: any) {
   let error: any = [];
   Object.keys(value).map((key: any) => {
     if (Object.keys(value).every((key) => schema.hasOwnProperty(key))) {
-      if (typeof value[key] != typeof schema[key]) {
+      if (typeof value[key] !== schema[key]) {
         const errorMessage = `Type of '${key}' (${typeof value[
           key
-        ]}) is different from the schema (${typeof schema[key]})`;
-        console.log(errorMessage);
-
-        error.push({
-          message: errorMessage,
-        });
+        ]}) is different from the schema (${schema[key]})`;
+        error.push(errorMessage);
       }
     }
   });
   return error;
 }
-compareTypes(obj1, obj2);
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -6,19 +6,33 @@ function delay(ms: number): Promise<void> {
 
 async function runTests() {
   const db = new microDB();
+  await db.initialize();
   db.schemaManager.registerSchema("user1", {
-    name: "string",
     age: "number",
+    name: "string",
     email: "string",
   });
 
+  // try {
+  //   console.log("Test 1: Insert a new key-value pair");
+  //   db.insert("user1", {
+  //     name: "alice",
+  //     age: 22,
+  //     email: "test@mail.com",
+  //   });
+  //   console.log("✔️ Passed: Successfully inserted key-value pair");
+  // } catch (error) {
+  //   if (error instanceof Error) {
+  //     console.log("❌ Failed: " + error.message);
+  //   } else {
+  //     console.log("❌ Failed: Unexpected error type");
+  //   }
+  // }
   try {
-    console.log("Test 1: Insert a new key-value pair");
-    db.insert("user1", {
-      name: "alice",
-      age: 22,
-      sex: "female",
-    });
+    console.log("Test 1: get a new key-value pair");
+    let user = db.get("user1");
+    console.log({ user });
+
     console.log("✔️ Passed: Successfully inserted key-value pair");
   } catch (error) {
     if (error instanceof Error) {
@@ -27,9 +41,8 @@ async function runTests() {
       console.log("❌ Failed: Unexpected error type");
     }
   }
-
   // Introduce a 3-second delay before updating
-  await delay(5000);
+  // await delay(5000);
 
   // try {
   //   console.log("Test 2: Update an existing key-value pair");
@@ -47,17 +60,17 @@ async function runTests() {
   //   }
   // }
 
-  try {
-    console.log("Test 3: delete an existing key-value pair");
-    db.delete("user1");
-    console.log("✔️ Passed: Successfully updated key-value pair");
-  } catch (error) {
-    if (error instanceof Error) {
-      console.log("❌ Failed: " + error.message);
-    } else {
-      console.log("❌ Failed: Unexpected error type");
-    }
-  }
+  // try {
+  //   console.log("Test 3: delete an existing key-value pair");
+  //   db.delete("user1");
+  //   console.log("✔️ Passed: Successfully updated key-value pair");
+  // } catch (error) {
+  //   if (error instanceof Error) {
+  //     console.log("❌ Failed: " + error.message);
+  //   } else {
+  //     console.log("❌ Failed: Unexpected error type");
+  //   }
+  // }
 }
 
 // Run the tests
